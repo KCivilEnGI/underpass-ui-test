71f5a2b5942119db6f2ac368147b3531b2e82e54
KCivilEnGI <kc.goomail@gmail.com>
2023-10-26 10:11:46 -0500

    code update
    Update the code to use the new feature, and set map center and URL to Wisconsin

--- src/App.js
+++ src/App.js
@@ -1,20 +1,65 @@
-import React, { useState, useRef } from "react";
+import React, { useState, useRef, useEffect } from "react";
+import {UnderpassFeatureList, UnderpassMap, HOTTheme} from "@hotosm/underpass-ui";
 import './App.css';
-import {UnderpassMap} from "@hotosm/underpass-ui";
+import "@hotosm/underpass-ui/dist/index.css";
+
+
+const config = {
+    MAPBOX_TOKEN: "",
+    API_URL:"http://74.207.226.67:8000" //get Wisconsin data
+}
 
 function App() {
-  const [coords, setCoords] = useState([-79.64696, 0.95953].reverse());
+  const [coords, setCoords] = useState([0.95953, -79.64696]);
     const [activeFeature, setActiveFeature] = useState(null);
-    const [tagKey, setTagKey] = useState("building");
-    const [tagValue, setTagValue] = useState("yes");
-    const tagKeyRef = useRef();
-    const tagValueRef = useRef();
+    const [tags, setTags] = useState("building=yes");
+    const [hashtag, setHashtag] = useState("");
+    const [mapSource, setMapSource] = useState("osm");
+    const [realtime, setRealtime] = useState(false);
+    const tagsInputRef = useRef("");
+    const hashtagInputRef = useRef("");
+    const styleSelectRef = useRef();
+
+
+    const hottheme = HOTTheme();
+
+    const defaultMapStyle = {
+      waysLine: {
+          ...hottheme.map.waysLine,
+          "line-opacity": .8,
+      },
+      waysFill: {
+          ...hottheme.map.waysFill,
+          "fill-opacity":
+          [
+              "match",
+              ["get", "type"],
+              "LineString", 0, .3
+          ]
+      },
+      nodesSymbol: {
+          ...hottheme.map.nodesSymbol,
+          "icon-opacity": [
+              "match",
+              ["get", "type"],
+              "Point", .8, 0
+            ],        
+      },
+    };
+
+    const [demoTheme, setDemoTheme] = useState({
+      map: defaultMapStyle
+    });
 
     const handleFilterClick = (e) => {
         e.preventDefault();
-        setTagKey(tagKeyRef.current.value);
-        setTagValue(tagValueRef.current.value);
+        setTags(tagsInputRef.current.value);
+        setHashtag(hashtagInputRef.current.value);
         return false;
+    }
+
+    const handleMapSourceSelect = (e) => {
+        setMapSource(e.target.options[e.target.selectedIndex].value);
     }
 
   return (
@@ -23,28 +68,70 @@ function App() {
           <form>
               <input
                   type="text"
-                  placeholder="key (ex: natural)"
-                  ref={tagKeyRef}
+                  placeholder="key (ex: building=yes)"
+                  ref={tagsInputRef}
               />
               &nbsp;
               <input
                   type="text"
-                  placeholder="value (ex: water)"
-                  ref={tagValueRef}
+                  placeholder="hashtag (ex: hotosm-project)"
+                  ref={hashtagInputRef}
               />
                &nbsp;
-              <button onClick={handleFilterClick}>Filter</button>
+              <button onClick={handleFilterClick}>Search</button>
           </form>
+          <select onChange={handleMapSourceSelect} ref={styleSelectRef}  className="mapSourceSelect">
+            <option value="osm">OSM</option>
+            <option value="bing">Bing</option>
+            <option value="esri">ESRI</option>
+          </select>
       </div>
       <div className="container">
           <div className="section2">
               <UnderpassMap
                   center={coords}
-                  tagKey={tagKey}
-                  tagValue={tagValue}
+                  tags={tags}
+                  hashtag={hashtag}
                   highlightDataQualityIssues
                   grayscale
                   popupFeature={activeFeature}
+                  source={mapSource}
+                  config={config}
+                  realtime={realtime}
+                  theme={demoTheme}
+
+              />
+          </div>
+          <div className="section1" style={{
+                    backgroundColor: `rgb(${hottheme.colors.white})`}}>
+              <h2>
+                <img src="/hot-logo.svg" />
+                <span>Latest mapped features</span>
+              </h2>
+              <form className="optionsForm">
+                <input onChange={() => { setRealtime(!realtime)}} name="liveCheckbox" type="checkbox" />
+                <label>Live</label>
+              </form>
+              <UnderpassFeatureList
+                  tags={tags}
+                  hashtag={hashtag}
+                  page={0}
+                  config={config}
+                  realtime={realtime}
+                  onSelect={(feature) => {
+                      setCoords([feature.lat, feature.lon]);
+                      const tags = JSON.stringify(feature.tags);
+                      const status = feature.status;
+                      setActiveFeature({properties: { tags, status } , ...feature});
+                  }}
+                  onUpdate={realtime ? (mostRecentFeature) => {
+                    if (mostRecentFeature) {
+                        setCoords([mostRecentFeature.lat, mostRecentFeature.lon]);
+                        const tags = JSON.stringify(mostRecentFeature.tags);
+                        const status = mostRecentFeature.status;
+                        setActiveFeature({properties: { tags, status } , ...mostRecentFeature});
+                    }
+                } : false}
               />
           </div>
       </div>
